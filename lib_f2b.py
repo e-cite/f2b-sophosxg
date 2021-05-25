ad91afada4390e5e72cda948734b37bc1031599e
Andreas Dolp <dev@andreas-dolp.de>
2021-05-25 22:40:36 +0200

    Improve comments and messages in lib_f2b.py.

--- lib_f2b.py
+++ lib_f2b.py
@@ -1,8 +1,9 @@
 from lib_sophosxg import *
 
+# Function called once at the start of Fail2Ban.
 def start():
-  print("Initial setup on f2b start")
-  print("Ensure IP host group", getConfig('sophos_iphostgroup_name'), "is present")
+  print("Start: Ensure IP host group", getConfig('sophos_iphostgroup_name'),
+    "is available.")
 
   # Get all elements of IpHostGroup
   xmldata = xml_getIpHostGroup()
@@ -19,26 +20,33 @@ def start():
   # If IP host group already present, do nothing
   # Otherwise add IP host group
   if found:
-    print("IP host group", getConfig('sophos_iphostgroup_name'), "already present")
+    print("Start: IP host group", getConfig('sophos_iphostgroup_name'),
+    "already available. Nothing to do.")
   else:
-    print("Adding IP host group", getConfig('sophos_iphostgroup_name'))
+    print("Start: IP host group", getConfig('sophos_iphostgroup_name'),
+    "not available. Adding it.")
     xmldata = xml_addIpHostGroup(getConfig('sophos_iphostgroup_name'))
     response = apiCall(xmldata)
 
   return 0
 
+# Function called once at the end of Fail2Ban
 def stop():
-  print("Cleanup on f2b stop")
-  print("Do NOT clean IP host group", getConfig('sophos_iphostgroup_name'),
-    "since this may affect existing firewall rules.")
+  print("Stop: Do NOT clean IP host group",
+    getConfig('sophos_iphostgroup_name'), "since this may affect",
+    "existing firewall rules.")
   return 0
 
+# Function called once before each actionban command
 def check():
-  print("Executed before each ban")
+  print("Check: There is nothing to check.")
   return 0
 
+# Function called once to flush (clear) all IPS, by shutdown
+# (resp. by stop of the jail or this action)
 def flush():
-  print("Flush (clear) all IPs, by shutdown or when stopping the jail")
+  print("Stop: Flushing all IPs in IP host group",
+    getConfig('sophos_iphostgroup_name'))
   # Get all elements of IpHostGroup
   xmldata = xml_getIpHostGroup()
   response = apiCall(xmldata)
@@ -67,10 +75,11 @@ def flush():
 
   return 0
 
+# Function called when banning an IP.
 def ban(ip):
   if not isValidIp(ip):
     return 1
-  print("Block single IP", ip)
+  print("Ban: Banning single IP", ip)
 
   # Add new IpHost as part of the IpHostGroup
   ipHostName = getConfig('sophos_iphost_prefix') + ip
@@ -79,10 +88,11 @@ def ban(ip):
 
   return 0
 
+# Function called when unbanning an IP.
 def unban(ip):
   if not isValidIp(ip):
     return 1
-  print("Unblock single IP", ip)
+  print("Unban: Unbanning single IP", ip)
 
   # Update IpHost to release any IpHostGroup bindings
   # Same request as adding an IpHost but without defining an IpHostGroup

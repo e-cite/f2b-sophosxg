9c3c52e0d338227b05ecabeb3d5d7f14aa597d0b
Andreas Dolp <dev@andreas-dolp.de>
2021-05-25 22:57:05 +0200

    Improve comments in lib_util.py.

--- lib_util.py
+++ lib_util.py
@@ -1,6 +1,9 @@
 import ipaddress
 import json
 
+# Open and read the config file
+# Arguments: Filename
+# Returns: config-dict or None on errors
 def readConfig(file):
   try:
     with open(file, 'r') as f:
@@ -9,10 +12,16 @@ def readConfig(file):
   except:
     return None
 
+# Getter function to get config parameters
+# Arguments: Requested parameter
+# Returns: Value of requested parameter
 def getConfig(param):
   config = readConfig('config.json')
   return config[param]
 
+# Checker function to proof the validity of an IP address
+# Arguments: IP address to be checked
+# Returns: 1 on valid, 0 on invalid
 def isValidIp(ip):
   try:
     ret = ipaddress.ip_address(ip)

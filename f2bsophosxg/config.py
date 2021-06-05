eb63824e49a91d23c936306d16365e08c078935c
Andreas Dolp <dev@andreas-dolp.de>
2021-06-05 23:17:01 +0200

    Improve error handling when loading config-file.

--- f2bsophosxg/config.py
+++ f2bsophosxg/config.py
@@ -6,7 +6,13 @@ from f2bsophosxg.libutil import (
 
 # Globally load the config
 global config
-config = readConfig('config.json')
+file = '/usr/local/etc/f2b-sophosxg/config.json'
+try:
+  config = readConfig(file)
+except FileNotFoundError:
+  print("Configuration file",file,"not found.")
+  sys.exit(1)
+
 if not isConfigValid(config):
   config = None
   sys.exit(1)
